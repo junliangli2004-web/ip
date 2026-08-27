b1980c3ef54d5fff0796ee6f63e205a3f1192152
Li Junliang <junliangli2004@gmail.com>
2026-08-27 19:52:41 +0800

    Update chatbot to greet and exit

---
+++ src/main/java/AllMind.java
@@ -0,0 +1,31 @@
+public class AllMind {
+    public static void main(String[] args) {
+        String banner =
+                "                                                                                      \n" +
+                        "                                                                                      \n" +
+                        "      db      `7MMF'      `7MMF'      `7MMM.     ,MMF'`7MMF'`7MN.   `7MF'`7MM\"\"\"Yb.   \n" +
+                        "     ;MM:       MM          MM          MMMb    dPMM    MM    MMN.    M    MM    `Yb. \n" +
+                        "    ,V^MM.      MM          MM          M YM   ,M MM    MM    M YMb   M    MM     `Mb \n" +
+                        "   ,M  `MM      MM          MM          M  Mb  M' MM    MM    M  `MN. M    MM      MM \n" +
+                        "   AbmmmqMA     MM      ,   MM      ,   M  YM.P'  MM    MM    M   `MM.M    MM     ,MP \n" +
+                        "  A'     VML    MM     ,M   MM     ,M   M  `YM'   MM    MM    M     YMM    MM    ,dP' \n" +
+                        ".AMA.   .AMMA..JMMmmmmMMM .JMMmmmmMMM .JML. `'  .JMML..JMML..JML.    YM  .JMMmmmdP'   \n" +
+                        "                                                                                      \n" +
+                        "                                                                                      ";
+
+        String horizontalLine = "________________________________________________________________________________________________________________________";
+
+        String welcomeMessage = "This is ALLMIND, a primitive chatbot system. \n" +
+                                "Welcome back, User";
+
+        String exitMessage = "It appears that will be all. Goodbye.";
+
+        System.out.println(horizontalLine);
+        System.out.println(banner);
+        System.out.println(welcomeMessage);
+        System.out.println(horizontalLine);
+        
+        System.out.println(exitMessage);
+        System.out.println(horizontalLine);
+    }
+}
